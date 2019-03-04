af7fa3261d7803d1b69a61b42ba630db714cecce
Logesh Palani <logeshmeena98@gmail.com>
2019-03-04 21:11:43 +0530

    Manual Commit

---
+++ IoTClient/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace IoTClient
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        IMobileServiceTable<powercontrol> userTable = App.MobileService.GetTable<powercontrol>();
+
+        private async void btnOn_Click(object sender, RoutedEventArgs e)
+        {
+                List<powercontrol> datalist = await App.MobileService.GetTable<powercontrol>().ToListAsync();
+                powercontrol obj = new powercontrol();
+                foreach (var x in datalist)
+                {
+                    obj.id = x.id;
+                    obj.status = "on";
+
+                }
+                await userTable.UpdateAsync(obj);
+            
+               
+        }
+
+        private async void btnOff_Click(object sender, RoutedEventArgs e)
+        {
+            List<powercontrol> datalist = await App.MobileService.GetTable<powercontrol>().ToListAsync();
+            powercontrol obj = new powercontrol();
+            foreach (var x in datalist)
+            {
+                obj.id = x.id;
+             obj.status = "off";
+
+            }
+            await userTable.UpdateAsync(obj);
+        }
+
+        private async void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                powercontrol obj1 = new powercontrol();
+
+                obj1.status = "on";
+
+                await userTable.InsertAsync(obj1);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
---
+++ IoTServer/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace IoTServer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private const int LED_PIN = 5;
+        private GpioPin pin;
+        private GpioPinValue pinValue;
+        private DispatcherTimer timer;
+        private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+        private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Tick += Timer_TickAsync;
+            InitGPIO();
+
+            if(pin != null)
+            {
+                timer.Start();
+            }
+        }
+
+        private void InitGPIO()
+        {
+            var gpio = GpioController.GetDefault();
+            if (gpio == null)
+            {
+                pin = null;
+                GpioStatus.Text = "There is no GPIO controller on this device";
+                return;
+            }
+            pin = gpio.OpenPin(LED_PIN);
+            pinValue = GpioPinValue.High;
+            pin.Write(pinValue);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+            GpioStatus.Text = "Gpio pin initialized correctly";
+                       
+        }
+        public string value;
+
+
+        private async void Timer_TickAsync(object sender, object e)
+        {
+            List<powercontrol> datalist = await App.MobileService.GetTable<powercontrol>().ToListAsync();
+            l1.ItemsSource = datalist;
+            foreach (var x in datalist)
+            {
+                value = x.status.ToString();
+            }
+            if (value =="on")
+            {
+                pinValue = GpioPinValue.Low;
+                pin.Write(pinValue);
+                LED.Fill = redBrush;
+                statusText.Text = "LED Truning On";
+            }
+            else
+            {
+                pinValue = GpioPinValue.High;
+                pin.Write(pinValue);
+                LED.Fill = grayBrush;
+            }
+        }
+    }
+}
